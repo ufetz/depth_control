5f65a59394712393904076ca0193e222fd03f64c
Thies Lennart Alff <lennart.alff@gmail.com>
2023-10-24 10:39:00 +0000

    some refactoring

--- nodes/depth_controller.py
+++ nodes/depth_controller.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-import rclpy
-
-from hippo_msgs.msg import ActuatorSetpoint, DepthStamped, Float64Stamped
-from rclpy.node import Node
 """
 This node is your depth controller.
 It takes as input a current depth and a given depth setpoint.
 Its output is a thrust command to the BlueROV's actuators.
 """
+import rclpy
+from hippo_msgs.msg import ActuatorSetpoint, DepthStamped, Float64Stamped
+from rclpy.node import Node
 
 
 class DepthControlNode(Node):
@@ -27,37 +26,58 @@ class DepthControlNode(Node):
         self.depth_sub = self.create_subscription(DepthStamped, 'depth',
                                                   self.on_depth, 1)
 
-    def on_setpoint(self, setpoint_msg):
+    def on_setpoint(self, setpoint_msg: Float64Stamped):
         # We received a new setpoint!
         # Let's save it, so that we can use it as soon as we receive
         # new depth data.
         self.current_setpoint = setpoint_msg.data
 
-    def on_depth(self, depth_msg):
+    def on_depth(self, depth_msg: DepthStamped):
         # We received a new depth message!
         # Now we can get to action!
         current_depth = depth_msg.depth
 
-        self.get_logger().info("Hi! I'm your controller running. " +
-                               f"I received a depth of {current_depth} m.",
-                               throttle_duration_sec=1)
+        self.get_logger().info(
+            f"Hi! I'm your controller running. "
+            f'I received a depth of {current_depth} m.',
+            throttle_duration_sec=1)
 
-        thrust_z = self.compute_control_output()
+        thrust = self.compute_control_output()
+        # either set the timestamp to the current time or set it to the
+        # stamp of `depth_msg` because the control output corresponds to this
+        # point in time. Both choices are meaningful.
+        # option 1:
+        # now = self.get_clock().now()
+        # option 2:
+        now = rclpy.time.Time.from_msg(depth_msg.header.stamp)
+        self.publish_vertical_thrust(thrust=thrust, now=now)
 
-        # Publish the thrust command
-        thrust_msg = ActuatorSetpoint()
-        thrust_msg.z = thrust_z
+    def publish_vertical_thrust(self, thrust: float,
+                                now: rclpy.time.Time) -> None:
+        msg = ActuatorSetpoint()
+        # we want to set the vertical thrust exlusively. mask out xy-components.
+        msg.ignore_x = True
+        msg.ignore_y = True
+        msg.ignore_z = False
+
+        msg.z = thrust
 
         # Let's add a time stamp
-        now = self.get_clock().now()
-        thrust_msg.header.stamp = now.to_msg()
+        msg.header.stamp = now.to_msg()
 
-        self.thrust_pub.publish(thrust_msg)
+        self.thrust_pub.publish(msg)
 
-    def compute_control_output(current_depth):
+    def compute_control_output(self, current_depth: float) -> float:
+        """Applies the PID control law.
+
+        Args:
+            current_depth: Required to compute the control error.
+
+        Returns: The control output is the desired thrust in vertical direction.
+        """
         # TODO: Do something...
 
-        thrust_z = 1.0  # This doesn't seem right yet...
+        thrust_z = 0.5  # This doesn't seem right yet...
         return thrust_z
 
 
